d2f4d6709bd8f3cb39ad0779544b836c7888b059
devbins <bin6160@126.com>
2020-04-10 21:57:17 +0800

    Style: pertty magit

--- lisp/init-pretty.el
+++ lisp/init-pretty.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 20
+;;     Update #: 22
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -397,16 +397,16 @@
   :hook(magit-mode . pretty-magit-setup)
   :config
   (pretty-magit-add-leaders
-   '(("Feature" ? (:foreground "slate gray" :height 1.2))
+   '(("Feature" ?🌟 (:foreground "slate gray" :height 1.2))
      ("Add"     ? (:foreground "#375E97" :height 1.2))
-     ("Fix"     ? (:foreground "#FB6542" :height 1.2))
+     ("Fix"     ?🐛 (:foreground "#FB6542" :height 1.2))
      ("Clean"   ? (:foreground "#FFBB00" :height 1.2))
      ("Perf"    ?🚀 (:foreground "#66bb6a" :height 1.2))
      ("Style"   ?🎨 (:foreground "#4fc3f7" :height 1.2))
      ("Test"    ?🔧 (:foreground "#8d6e63" :height 1.2))
      ("Refactor" ?🔨 (:foreground "#ef6c00" :height 1.2))
      ("Chore"   ?🏠 (:foreground "#9c27b0" :height 1.2))
-     ("Docs"    ? (:foreground "#3F681C" :height 1.2)))))
+     ("Docs"    ?📝 (:foreground "#3F681C" :height 1.2)))))
 
 (provide 'init-pretty)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
