01160dce34d4976dac962b4346e1330f7d50ef0d
devbins <bin6160@126.com>
2020-04-26 20:23:10 +0800

    Feature: set bongo mode initial state

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 32
+;;     Update #: 33
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -101,6 +101,7 @@
   (evil-set-initial-state 'esup-mode 'emacs)
   (evil-set-initial-state 'diff-mode 'normal)
   (evil-set-initial-state 'deft-mode 'emacs)
+  (evil-set-initial-state 'bongo-mode 'emacs)
   (evil-set-initial-state 'leetcode--problems-mode 'emacs)
   (evil-set-initial-state 'image-mode 'motion)
   (evil-set-initial-state 'image-dired-minor-mode 'emacs)
