4008119b8c854c2614bcca7723487bcdba9a3d43
devbins <bin6160@126.com>
2020-05-10 14:52:02 +0800

    Style: font

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -377,26 +377,26 @@ If FRAME is nil, it defaults to the selected frame."
 
 
 ;; Fonts
-(when (display-graphic-p)
-  ;; Set default font
-  (cl-loop for font in '("SF Mono" "Hack" "Source Code Pro" "Fira Code" "Fira Code Symbol" "Operator Mono Lig"
-                      "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
-           when (font-installed-p font)
-           return (set-face-attribute 'default nil
-                                  :font font
-                                  :height (cond (sys/mac-x-p 130)
-                                                (sys/win32p 110)
-                                                (t 100))))
+;; (when (display-graphic-p)
+;;   ;; Set default font
+;;   (cl-loop for font in '("SF Mono" "Hack" "Source Code Pro" "Fira Code" "Fira Code Symbol"
+;;                       "Menlo" "Monaco" "DejaVu Sans Mono" "Consolas")
+;;            when (font-installed-p font)
+;;            return (set-face-attribute 'default nil
+;;                                   :font font
+;;                                   :height (cond (sys/mac-x-p 130)
+;;                                                 (sys/win32p 110)
+;;                                                 (t 100))))
 
-  ;; Specify font for all unicode characters
-  (cl-loop for font in '("Symbola" "Apple Symbols" "Symbol" "icons-in-terminal")
-           when (font-installed-p font)
-           return (set-fontset-font t 'unicode font nil 'prepend))
+;;   ;; Specify font for all unicode characters
+;;   (cl-loop for font in '("Symbola" "Apple Symbols" "Symbol" "icons-in-terminal")
+;;            when (font-installed-p font)
+;;            return (set-fontset-font t 'unicode font nil 'prepend))
 
-  ;; Specify font for Chinese characters
-  (cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
-           when (font-installed-p font)
-           return (set-fontset-font t '(#x4e00 . #x9fff) font)))
+;;   ;; Specify font for Chinese characters
+;;   (cl-loop for font in '("WenQuanYi Micro Hei" "Microsoft Yahei")
+;;            when (font-installed-p font)
+;;            return (set-fontset-font t '(#x4e00 . #x9fff) font)))
 
 
 (defun set-monospaced-font (english chinese english-size chinese-size)
