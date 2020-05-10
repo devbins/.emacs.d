8e36b5952e6aa5ce3e5b400864d9bc63b35d2b38
devbins <bin6160@126.com>
2020-05-10 14:52:48 +0800

    Style: treemacs

--- lisp/init-treemacs.el
+++ lisp/init-treemacs.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 11
+;;     Update #: 14
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -64,36 +64,13 @@
          ([mouse-1]   . treemacs-single-click-expand-action))
   :config
   (setq treemacs-collapse-dirs           (if treemacs-python-executable 3 0)
-        treemacs-sorting                 'alphabetic-case-insensitive-asc
+        treemacs-sorting                 'alphabetic-asc
+        treemacs-follow-after-init       t
         treemacs-is-never-other-window   t
         treemacs-silent-filewatch        t
         treemacs-silent-refresh          t
+        treemacs-width                   30)
 
-        treemacs-deferred-git-apply-delay   0.5
-        treemacs-display-in-side-window     t
-        treemacs-file-event-delay           5000
-        treemacs-file-follow-delay          0.2
-        treemacs-follow-after-init          t
-        treemacs-follow-recenter-distance   0.1
-        treemacs-git-command-pipe           ""
-        treemacs-goto-tag-strategy          'refetch-index
-        treemacs-indentation                2
-        treemacs-indentation-string         " "
-        treemacs-max-git-entries            5000
-        treemacs-no-png-images              nil
-        treemacs-no-delete-other-windows    t
-        treemacs-project-follow-cleanup     nil
-        treemacs-persist-file               (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-        treemacs-recenter-after-file-follow nil
-        treemacs-recenter-after-tag-follow  nil
-        treemacs-show-cursor                nil
-        treemacs-show-hidden-files          t
-        treemacs-space-between-root-nodes   t
-        treemacs-tag-follow-cleanup         t
-        treemacs-tag-follow-delay           1.5
-        treemacs-width                   35)
-
-  :config
   (treemacs-follow-mode t)
   (treemacs-filewatch-mode t)
   (pcase (cons (not (null (executable-find "git")))
@@ -105,11 +82,6 @@
 
   (use-package treemacs-evil
     :demand t)
-
-  (use-package treemacs-icons-dired
-    :after treemacs dired
-    :ensure t
-    :config (treemacs-icons-dired-mode))
 
   ;; Projectile integration
   (use-package treemacs-projectile
