727898118267e81fd9bd55266cd915341b359820
devbins <bin6160@126.com>
2020-05-11 22:52:17 +0800

    Clean: remove paradox

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 6
+;;     Update #: 7
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -84,28 +84,6 @@
 
 ;; Update GPG keyring for GNU ELPA
 (use-package gnu-elpa-keyring-update)
-
-;; A modern Packages Menu
-(use-package paradox
-  :init
-  (setq paradox-execute-asynchronously t
-        paradox-github-token t
-        paradox-display-star-count nil)
-
-  ;; Replace default `list-packages'
-  (defun my-paradox-enable (&rest _)
-    "Enable paradox, overriding the default package-menu."
-    (paradox-enable))
-  (advice-add #'list-packages :before #'my-paradox-enable)
-  :config
-  (when (fboundp 'page-break-lines-mode)
-    (add-hook 'paradox-after-execute-functions
-              (lambda (&rest _)
-                (let ((buf (get-buffer-create "*Paradox Report*"))
-                      (inhibit-read-only t))
-                  (with-current-buffer buf
-                    (page-break-lines-mode 1))))
-              t)))
 
 
 (provide 'init-package)
