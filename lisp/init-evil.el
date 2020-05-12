a001c502d1adefb1efe11169e689ac14f7b910c7
devbins <bin6160@126.com>
2020-05-12 09:03:24 +0800

    Style: align

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 57
+;;     Update #: 69
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -122,14 +122,14 @@
   ;; but [escape] should switch back to normal state
   (define-key evil-insert-state-map [escape] 'evil-normal-state)
 
-  (define-key evil-normal-state-map [escape] 'keyboard-quit)
-  (define-key evil-visual-state-map [escape] 'keyboard-quit)
-  (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-  (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+  (define-key evil-normal-state-map [escape]           'keyboard-quit)
+  (define-key evil-visual-state-map [escape]           'keyboard-quit)
+  (define-key minibuffer-local-map [escape]            'minibuffer-keyboard-quit)
+  (define-key minibuffer-local-ns-map [escape]         'minibuffer-keyboard-quit)
   (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
   (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-  (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-  (global-set-key [escape] 'evil-exit-emacs-state)
+  (define-key minibuffer-local-isearch-map [escape]    'minibuffer-keyboard-quit)
+  (global-set-key [escape]                             'evil-exit-emacs-state)
 
   (evil-define-key 'normal help-mode-map
     "q" 'quit-window)
@@ -138,26 +138,22 @@
     "q" 'quit-window))
 
 (use-package evil-leader
-  :defer t
   :hook (after-init . global-evil-leader-mode)
   :config
   (evil-leader/set-leader "<SPC>")
   (evil-leader/set-key
     "SPC" 'counsel-M-x
 
-    "1" 'winum-select-window-1
-    "2" 'winum-select-window-2
-    "3" 'winum-select-window-3
-    "4" 'winum-select-window-4
-    "5" 'winum-select-window-5
-    "6" 'winum-select-window-6
-    "7" 'winum-select-window-7
-    "8" 'winum-select-window-8
-    "9" 'winum-select-window-9
-    "0" 'treemacs-select-window
-
-
-    "//" 'org-sparse-tree
+    "1"   'winum-select-window-1
+    "2"   'winum-select-window-2
+    "3"   'winum-select-window-3
+    "4"   'winum-select-window-4
+    "5"   'winum-select-window-5
+    "6"   'winum-select-window-6
+    "7"   'winum-select-window-7
+    "8"   'winum-select-window-8
+    "9"   'winum-select-window-9
+    "0"   'treemacs-select-window
 
     "ayl" 'ein:notebooklist-login
     "ayo" 'ein:notebooklist-open
@@ -169,131 +165,132 @@
     "aot" 'org-todo-list
     "am"  'wl
 
-    "bb" 'ivy-switch-buffer
-    "bd" 'kill-this-buffer
-    "bm" 'bookmark-set
-    "bj" 'bookmark-jump
-    "bI" 'ibuffer
-    "bp" 'previous-buffer
-    "bn" 'next-buffer
-    "bt" 'imenu-list-smart-toggle
-    "bs" 'switch-to-scratch-buffer
+    "bb"  'ivy-switch-buffer
+    "bd"  'kill-this-buffer
+    "bm"  'bookmark-set
+    "bj"  'bookmark-jump
+    "bI"  'ibuffer
+    "bp"  'previous-buffer
+    "bn"  'next-buffer
+    "bt"  'imenu-list-smart-toggle
+    "bs"  'switch-to-scratch-buffer
 
-    "ff" 'counsel-find-file
-    "fj" 'dired-jump
+    "ff"  'counsel-find-file
+    "fj"  'dired-jump
     "fCd" 'unix2dos
     "fCu" 'dos2unix
-    "fE" 'sudo-edit
-    "fd" 'delete-this-file
-    "ft" 'treemacs
-    "fo" 'open-file-or-directory-in-external-app
-    "fr" 'counsel-recentf
-    "fR" 'rename-this-file
-    "fs" 'save-buffer
-    "fa" 'save-some-buffers
+    "fE"  'sudo-edit
+    "fd"  'delete-this-file
+    "ft"  'treemacs
+    "fo"  'open-file-or-directory-in-external-app
+    "fr"  'counsel-recentf
+    "fR"  'rename-this-file
+    "fs"  'save-buffer
+    "fa"  'save-some-buffers
     "fyy" 'copy-file-name
 
-    "Fb" 'switch-to-buffer-other-frame
-    "FB" 'display-buffer-other-frame
-    "Fd" 'delete-frame
-    "FD" 'delete-other-frames
-    "Fn" 'make-frame
-    "Fo" 'other-frame
-    "FO" 'dired-other-frame
-    "Ff" 'find-file-other-frame
+    "Fb"  'switch-to-buffer-other-frame
+    "FB"  'display-buffer-other-frame
+    "Fd"  'delete-frame
+    "FD"  'delete-other-frames
+    "Fn"  'make-frame
+    "Fo"  'other-frame
+    "FO"  'dired-other-frame
+    "Ff"  'find-file-other-frame
 
-    "gs" 'magit-status
-    "gt" 'git-timemachine
-    "gb" 'magit-blame
-    "gp" 'git-messenger:popup-message
+    "gs"  'magit-status
+    "gt"  'git-timemachine
+    "gb"  'magit-blame
+    "gp"  'git-messenger:popup-message
 
-    "hf" 'describe-function
-    "hk" 'describe-key
-    "hv" 'describe-variable
-    "hp" 'describe-package
+    "hf"  'describe-function
+    "hk"  'describe-key
+    "hv"  'describe-variable
+    "hp"  'describe-package
 
-    "Ti" 'org-toggle-inline-images
-    "Tl" 'org-toggle-link-display
+    "Ti"  'org-toggle-inline-images
+    "Tl"  'org-toggle-link-display
 
-    "nf" 'narrow-to-defun
-    "np" 'narrow-to-page
-    "nr" 'narrow-to-region
-    "nw" 'widen
+    "nf"  'narrow-to-defun
+    "np"  'narrow-to-page
+    "nr"  'narrow-to-region
+    "nw"  'widen
 
     ;; Project
-    "pb" 'projectile-switch-to-buffer
-    "pc" 'projectile-compile-project
-    "pd" 'projectile-find-dir
-    "pe" 'projectile-edit-dir-locals
-    "pf" 'projectile-find-file
-    "pg" 'projectile-find-tag
-    "pk" 'projectile-kill-buffers
-    "pp" 'projectile-switch-project
-    "pr" 'projectile-recentf
-    "pR" 'projectile-replace
-    "ps" 'projectile-switch-project
-    "pv" 'projectile-vc
-    "px" 'projectile-remove-known-project
-    "pz" 'projectile-cache-current-file
+    "pb"  'projectile-switch-to-buffer
+    "pc"  'projectile-compile-project
+    "pd"  'projectile-find-dir
+    "pe"  'projectile-edit-dir-locals
+    "pf"  'projectile-find-file
+    "pg"  'projectile-find-tag
+    "pk"  'projectile-kill-buffers
+    "pp"  'projectile-switch-project
+    "pr"  'projectile-recentf
+    "pR"  'projectile-replace
+    "ps"  'projectile-switch-project
+    "pv"  'projectile-vc
+    "px"  'projectile-remove-known-project
+    "pz"  'projectile-cache-current-file
 
-    "Pc" 'password-store-copy
-    "Pg" 'password-store-generate
-    "Pr" 'password-store-remove
-    "Pe" 'password-store-edit
-    "PR" 'password-store-rename
-    "Pi" 'password-store-insert
-    "Pu" 'password-store-url
+    "Pc"  'password-store-copy
+    "Pg"  'password-store-generate
+    "Pr"  'password-store-remove
+    "Pe"  'password-store-edit
+    "PR"  'password-store-rename
+    "Pi"  'password-store-insert
+    "Pu"  'password-store-url
 
-    "sa" 'mark-whole-buffer
-    "sp" 'projectile-ripgrep
-    "ss" 'swiper
-    "sn" 'snails
+    "sa"  'mark-whole-buffer
+    "sp"  'projectile-ripgrep
+    "ss"  'swiper
+    "sn"  'snails
 
-    "tl" 'toggle-truncate-lines
-    "tw" 'toggle-frame-maximized
-    "tf" 'toggle-frame-fullscreen
+    "tl"  'toggle-truncate-lines
+    "tw"  'toggle-frame-maximized
+    "tf"  'toggle-frame-fullscreen
 
-    "ww" 'other-window
-    "wm" 'toggle-maximize-buffer
-    "wo" 'other-frame
-    "w/" 'split-window-right
-    "w-" 'split-window-below
-    "wd" 'delete-window
-    "w=" 'balance-windows
-    "wh" 'evil-window-left
-    "wH" 'evil-window-move-far-left
-    "wl" 'evil-window-right
-    "wL" 'evil-window-move-far-right
-    "wj" 'evil-window-down
-    "wJ" 'evil-window-move-very-bottom
-    "wk" 'evil-window-up
-    "wK" 'evil-window-move-very-top
+    "ww"  'other-window
+    "wm"  'toggle-maximize-buffer
+    "wo"  'other-frame
+    "w/"  'split-window-right
+    "w-"  'split-window-below
+    "wd"  'delete-window
+    "w="  'balance-windows
+    "wh"  'evil-window-left
+    "wH"  'evil-window-move-far-left
+    "wl"  'evil-window-right
+    "wL"  'evil-window-move-far-right
+    "wj"  'evil-window-down
+    "wJ"  'evil-window-move-very-bottom
+    "wk"  'evil-window-up
+    "wK"  'evil-window-move-very-top
 
-    "v" 'er/expand-region
+    "v"   'er/expand-region
 
-    "xu" 'downcase-region
-    "xU" 'upcase-region
-    "u" 'downcase-word
-    "U" 'upcase-word
+    "xu"  'downcase-region
+    "xU"  'upcase-region
+    "u"   'downcase-word
+    "U"   'upcase-word
 
-    "yp" 'youdao-dictionary-search-at-point-posframe
-    "yi" 'youdao-dictionary-search-from-input
+    "yp"  'youdao-dictionary-search-at-point-posframe
+    "yi"  'youdao-dictionary-search-from-input
 
-    "qq" 'save-buffers-kill-terminal
-    "qr" 'devbins/restart-emacs
+    "qq"  'save-buffers-kill-terminal
+    "qr"  'devbins/restart-emacs
 
-    "'" 'shell-pop
+    "     '" 'shell-pop
     "TAB" 'switch-to-prev-buffer))
 
 
 (use-package evil-lisp-state
-  :defer t
   :init
   (setq evil-lisp-state-global t)
-  :hook (prog-mode . (lambda ()(require 'evil-lisp-state)))
+  :hook (prog-mode . evil-lisp-state)
   :config
   (evil-lisp-state-leader "SPC k"))
 
+;; 对齐
+;; https://github.com/edkolev/evil-lion/tree/6b03593f5dd6e7c9ca02207f9a73615cf94c93ab
 (use-package evil-lion
   :hook (after-init . evil-lion-mode))
 
@@ -306,8 +303,8 @@
   :config
   (evil-define-key 'operator global-map "s" 'evil-surround-edit)
   (evil-define-key 'operator global-map "S" 'evil-Surround-edit)
-  (evil-define-key 'visual global-map "S" 'evil-surround-region)
-  (evil-define-key 'visual global-map "gS" 'evil-Surround-region)
+  (evil-define-key 'visual global-map "S"   'evil-surround-region)
+  (evil-define-key 'visual global-map "gS"  'evil-Surround-region)
   :hook (after-init . global-evil-surround-mode))
 
 ;; esc quits
