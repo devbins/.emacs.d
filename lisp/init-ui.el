0d709ab51416da2fda834ed85f6ebadc10691724
devbins <bin6160@126.com>
2020-09-11 21:05:14 +0800

    Feature: set default transparency

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -271,12 +271,12 @@
         beacon-blink-delay .2          ; default .3
         beacon-size 20))
 
-(defvar active-transparency 90
+(defvar active-transparency 80
   "A value from the range (0..100), in increasing opacity, which describes the
 transparency level of a frame when it's active or selected. Transparency
 can be toggled through `toggle-transparency'.")
 
-(defvar inactive-transparency 60
+(defvar inactive-transparency 75
   "A value from the range (0..100), in increasing opacity, which describes the
 transparency level of a frame when it's inactive or deselected. Transparency
 can be toggled through `toggle-transparency'.")
