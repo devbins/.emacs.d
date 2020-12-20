ccf280091de9b1e3ae46678674272773fa45a10e
devbins <bin6160@126.com>
2020-12-20 12:32:10 +0800

    Feature: optimized tab key binding in comapny mode with yasnippet

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 37
+;;     Update #: 42
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -58,6 +58,8 @@
          ("C-p" . company-select-previous)
          ("C-n" . company-select-next)
          ("C-M-/" . my-company-yasnippet)
+         ("<tab>" . tab-complete)
+         ("C-s" . company-filter-candidates)
          ;; ("C-c C-y" . my-company-yasnippet)
          :map company-search-map
          ("C-p" . company-select-previous)
@@ -77,49 +79,29 @@
         company-require-match nil
         completion-ignore-case t
         company-dabbrev-ignore-case t
+        company-show-numbers t
         company-dabbrev-downcase nil
         company-global-modes '(not erc-mode message-mode help-mode gud-mode eshell-mode shell-mode)
         company-backends '((company-capf :with company-yasnippet)
                            (company-dabbrev-code company-keywords company-files)
                            company-dabbrev))
   :config
-  ;; `yasnippet' integration
-  (with-no-warnings
-    (with-eval-after-load 'yasnippet
-      (defun company-backend-with-yas (backend)
-        "Add `yasnippet' to company backend."
-        (if (and (listp backend) (member 'company-yasnippet backend))
-            backend
-          (append (if (consp backend) backend (list backend))
-                  '(:with company-yasnippet))))
+  (defun tab-complete ()
+    "Try to `org-cycle', `yas-expand', and `yas-next-field' at current cursor position.
 
-      (defun my-company-enbale-yas (&rest _)
-        "Enable `yasnippet' in `company'."
-        (setq company-backends (mapcar #'company-backend-with-yas company-backends)))
-
-      (defun my-lsp-fix-company-capf ()
-        "Remove redundant `comapny-capf'."
-        (setq company-backends
-              (remove 'company-backends (remq 'company-capf company-backends))))
-      (advice-add #'lsp-completion--enable :after #'my-lsp-fix-company-capf)
-
-      (defun my-company-yasnippet-disable-inline (fun command &optional arg &rest _ignore)
-        "Enable yasnippet but disable it inline."
-        (if (eq command 'prefix)
-            (when-let ((prefix (funcall fun 'prefix)))
-              (unless (memq (char-before (- (point) (length prefix)))
-                            '(?. ?< ?> ?\( ?\) ?\[ ?{ ?} ?\" ?' ?`))
-                prefix))
-          (progn
-            (when (and (bound-and-true-p lsp-mode)
-                     arg (not (get-text-property 0 'yas-annotation-patch arg)))
-              (let* ((name (get-text-property 0 'yas-annotation arg))
-                     (snip (format "%s (Snippet)" name))
-                     (len (length arg)))
-                (put-text-property 0 len 'yas-annotation snip arg)
-                (put-text-property 0 len 'yas-annotation-patch t arg)))
-            (funcall fun command arg))))
-      (advice-add #'company-yasnippet :around #'my-company-yasnippet-disable-inline)))
+If all failed, try to complete the common part with `company-complete-common'"
+    (interactive)
+    (if yas-minor-mode
+        (let ((old-point (point))
+              (old-tick (buffer-chars-modified-tick))
+              (func-list '(org-cycle yas-expand yas-next-field)))
+          (catch 'func-suceed
+            (dolist (func func-list)
+              (ignore-errors (call-interactively func))
+              (unless (and (eq old-point (point))
+                         (eq old-tick (buffer-chars-modified-tick)))
+                (throw 'func-suceed t)))
+            (company-complete-common)))))
   ;; Better sorting and filtering
   (use-package company-prescient
     :init (company-prescient-mode 1))
