4ff8cdb7e6c72389b9a3ae416eea8abdcd8f23a4
devbins <bin6160@126.com>
2021-07-13 07:20:27 +0800

    Add: auto insert template

---
+++ auto-insert-template/template.h
@@ -0,0 +1,35 @@
+/*
+ * File:    `(buffer-name)`
+ * Author:  `user-full-name` <`user-mail-address`>
+ * Brief:   ${0:brief}
+ *
+ * Copyright (c) `user-full-name`
+ *
+ * Licensed under the Academic Free License version 2.1
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ * History:
+ * ================================================================
+ * `(format-time-string "%Y-%m-%d %H:%M"(current-time))` `user-full-name` <`user-mail-address`> created.
+ */
+
+#ifndef ${1:_`(upcase (file-name-nondirectory (file-name-sans-extension (buffer-file-name))))`_H}
+#define $1
+
+
+#endif /* $1 */
