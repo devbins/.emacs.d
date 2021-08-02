6a06df6f1911f48fde1bd4479c7ca2239302b072
devbins <bin6160@126.com>
2021-08-02 08:46:20 +0800

    Feature: optimized highlight

--- lisp/init-highlight.el
+++ lisp/init-highlight.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 3
+;;     Update #: 9
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -146,12 +146,47 @@ FACE defaults to inheriting from default and highlight."
 
 ;; Highlight indentions
 (use-package highlight-indent-guides
-  :if (display-graphic-p)
   :diminish
+  :hook (prog-mode . highlight-indent-guides-mode)
   :init (setq highlight-indent-guides-responsive 'top
-              highlight-indent-guides-delay 0
-              highlight-indent-guides-auto-character-face-perc 7
-              highlight-indent-guides-method 'character))
+              highlight-indent-guides-suppress-auto-error t
+              highlight-indent-guides-method 'character)
+  :config
+  (with-no-warnings
+    ;; Don't display first level of indentation
+    (defun my-indent-guides-for-all-but-first-column (level responsive display)
+      (unless (< level 1)
+        (highlight-indent-guides--highlighter-default level responsive display)))
+    (setq highlight-indent-guides-highlighter-function
+          #'my-indent-guides-for-all-but-first-column)
+
+    ;; Disable in `macrostep' expanding
+    (with-eval-after-load 'macrostep
+      (advice-add #'macrostep-expand
+                  :after (lambda (&rest _)
+                           (when highlight-indent-guides-mode
+                             (highlight-indent-guides-mode -1))))
+      (advice-add #'macrostep-collapse
+                  :after (lambda (&rest _)
+                           (when (derived-mode-p 'prog-mode)
+                             (highlight-indent-guides-mode 1)))))
+
+    ;; Don't display indentations in `swiper'
+    ;; https://github.com/DarthFennec/highlight-indent-guides/issues/40
+    (with-eval-after-load 'ivy
+      (defun my-ivy-cleanup-indentation (str)
+        "Clean up indentation highlighting in ivy minibuffer."
+        (let ((pos 0)
+              (next 0)
+              (limit (length str))
+              (prop 'highlight-indent-guides-prop))
+          (while (and pos next)
+            (setq next (text-property-not-all pos limit prop nil str))
+            (when next
+              (setq pos (text-property-any next limit prop nil str))
+              (ignore-errors
+                (remove-text-properties next pos '(display nil face nil) str))))))
+      (advice-add #'ivy-cleanup-string :after #'my-ivy-cleanup-indentation))))
 
 ;; Colorize color names in buffers
 (use-package rainbow-mode
@@ -222,9 +257,6 @@ FACE defaults to inheriting from default and highlight."
   (setq diff-hl-fringe-bmp-function #'my-diff-hl-fringe-bmp-function)
 
   (unless (display-graphic-p)
-    (setq diff-hl-margin-symbols-alist
-          '((insert . " ") (delete . " ") (change . " ")
-            (unknown . " ") (ignored . " ")))
     ;; Fall back to the display margin since the fringe is unavailable in tty
     (diff-hl-margin-mode 1)
     ;; Avoid restoring `diff-hl-margin-mode'
