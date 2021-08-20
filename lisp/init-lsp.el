247cddea95105a2c8b4c786235cf91c5e390643d
devbins <bin6160@126.com>
2021-08-20 08:38:45 +0800

    Feature: Support oc

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -270,10 +270,10 @@
                   projectile-project-root-files-top-down-recurring))))
 
 ;; Swift/C/C++/Objective-C
-(when sys/macp
-  (use-package lsp-sourcekit
-    :init (setq lsp-sourcekit-executable
-                "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp")))
+(use-package lsp-sourcekit
+  :if sys/macp
+  :init (setq lsp-sourcekit-executable
+              "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp"))
 
 ;; Java support
 (use-package lsp-java
