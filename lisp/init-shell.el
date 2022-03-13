e993295d6ff846cac5f784440d968b47403851e7
devbins <bin6160@126.com>
2022-03-13 17:35:40 +0800

    Feature: optimized shell

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 7
+;;     Update #: 8
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -116,7 +116,61 @@
            (executable-find "cmake")
            (executable-find "libtool")
            (executable-find "make"))
-  (use-package vterm))
+  (use-package vterm
+    :commands vterm--internal
+    :bind (:map vterm-mode-map
+           ([f9] . (lambda ()
+                     (interactive)
+                     (and (fboundp 'shell-pop)
+                          (shell-pop nil)))))
+    :init
+    (setq vterm-always-compile-module t)
+
+    (with-no-warnings
+      (when (childframe-workable-p)
+        (defvar vterm-posframe--frame nil)
+
+        (defun vterm-posframe-hidehandler (_)
+          "Hidehandler used by `vterm-posframe-toggle'."
+          (not (eq (selected-frame) posframe--frame)))
+
+        (defun vterm-posframe-toggle ()
+          "Toggle `vterm' child frame."
+          (interactive)
+          (let ((buffer (vterm--internal #'ignore 100)))
+            (if (and vterm-posframe--frame
+                     (frame-live-p vterm-posframe--frame)
+                     (frame-visible-p vterm-posframe--frame))
+                (progn
+                  (posframe-hide buffer)
+                  ;; Focus the parent frame
+                  (select-frame-set-input-focus (frame-parent vterm-posframe--frame)))
+              (let ((width  (max 80 (/ (frame-width) 2)))
+                    (height (/ (frame-height) 2)))
+                (setq vterm-posframe--frame
+                      (posframe-show
+                       buffer
+                       :poshandler #'posframe-poshandler-frame-center
+                       :hidehandler #'vterm-posframe-hidehandler
+                       :left-fringe 8
+                       :right-fringe 8
+                       :width width
+                       :height height
+                       :min-width width
+                       :min-height height
+                       :internal-border-width 3
+                       :internal-border-color (face-foreground 'font-lock-comment-face nil t)
+                       :background-color (face-background 'tooltip nil t)
+                       :override-parameters '((cursor-type . t))
+                       :accept-focus t))
+                ;; Blink cursor
+                (with-current-buffer buffer
+                  (save-excursion
+                    (vterm-clear t))
+                  (setq-local cursor-type 'box))
+                ;; Focus the child frame
+                (select-frame-set-input-focus vterm-posframe--frame)))))
+        (bind-key "C-`" #'vterm-posframe-toggle)))))
 
 ;; Shell Pop
 (use-package shell-pop
