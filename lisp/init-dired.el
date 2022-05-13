ad225a6a5df9eabd4f72a4e5c7d3cc4707a2ea1b
devbins <bin6160@126.com>
2022-05-13 18:53:34 +0800

    Clean: delete dirvish

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -231,37 +231,6 @@
             "w" 'image-transform-fit-to-width
             "s" 'image-transform-set-scale))
 
-(use-package dirvish
-  :custom
-  ;; Feel free to replace `all-the-icons' with `vscode-icon'.
-  (dirvish-attributes '(expanded-state all-the-icons file-size))
-  ;; Maybe the icons are too big to your eyes
-  ;; (dirvish-all-the-icons-height 0.8)
-
-  ;; List directories that has over 10000 files asynchronously
-  ;; This feature is disabled by default
-  ;; (dirvish-async-listing-threshold 10000)
-  ;; Place this line under :init to ensure the overriding at startup, see #22
-  (dirvish-side-follow-buffer-file nil)
-  (dirvish-override-dired-mode)
-  :config
-  (dirvish-peek-mode)
-  ;; In case you want the details at startup like `dired'
-  ;; :hook
-  ;; (dirvish-mode . (lambda () (dired-hide-details-mode -1)))
-  :bind
-  (nil ; Bind `dirvish', `dirvish-dired' and `dirvish-side' as you see fit
-   :map dired-mode-map
-   ("SPC" . dirvish-show-history)
-   ("r"   . dirvish-roam)
-   ("b"   . dirvish-goto-bookmark)
-   ("f"   . dirvish-file-info-menu)
-   ("M-a" . dirvish-mark-actions-menu)
-   ("M-s" . dirvish-setup-menu)
-   ("M-f" . dirvish-toggle-fullscreen)
-   ([remap dired-summary] . dirvish-dispatch)
-   ([remap dired-do-copy] . dirvish-yank)
-   ([remap mode-line-other-buffer] . dirvish-other-buffer)))
 
 (provide 'init-dired)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
