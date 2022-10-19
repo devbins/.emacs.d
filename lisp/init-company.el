294a33a071f59a298e42b597b0443fd2364df03b
devbins <bin6160@126.com>
2022-10-19 21:26:43 +0800

    Clean: remove company-tabnine

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 64
+;;     Update #: 66
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -45,56 +45,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Code:
-
-
-(use-package company-tabnine
-  :disabled
-  :after company
-  :config
-  (defun tabnine//merge-company-tabnine-to-company-lsp ()
-    (when (memq 'company-lsp company-backends)
-      (setq-local company-backends (remove 'company-lsp company-backends))
-      (add-to-list 'company-backends '(company-lsp :with company-tabnine :separate))))
-
-  (defun tabnine//company-box-icons--tabnine (candidate)
-    (when (eq (get-text-property 0 'company-backend candidate)
-              'company-tabnine)
-      'Reference))
-
-  (defun tabnine//sort-by-tabnine (candidates)
-    "The first two candidates will be from company-lsp, the following two
-candidates will be from company-tabnine, others keeping their own origin order."
-    (if (or (functionp company-backend)
-           (not (and (listp company-backend) (memq 'company-tabnine company-backend))))
-        candidates
-      (let ((candidates-table (make-hash-table :test #'equal))
-            candidates-1
-            candidates-2)
-        (dolist (candidate candidates)
-          (if (eq (get-text-property 0 'company-backend candidate)
-                  'company-tabnine)
-              (unless (gethash candidate candidates-table)
-                (push candidate candidates-2))
-            (push candidate candidates-1)
-            (puthash candidate t candidates-table)))
-        (setq candidates-1 (nreverse candidates-1))
-        (setq candidates-2 (nreverse candidates-2))
-        (nconc (seq-take candidates-1 2)
-               (seq-take candidates-2 2)
-               (seq-drop candidates-1 2)
-               (seq-drop candidates-2 2)))))
-
-  (add-to-list 'company-backends #'company-tabnine)
-
-  (defadvice company-echo-show (around disable-tabnine-upgrade-message activate)
-    (let ((company-message-func (ad-get-arg 0)))
-      (when (and company-message-func
-               (stringp (funcall company-message-func)))
-        (unless (string-match "The free version of TabNine only indexes up to" (funcall company-message-func))
-          ad-do-it))))
-
-  (with-eval-after-load 'lsp-mode
-    (advice-add 'lsp :after #'tabnine//merge-company-tabnine-to-company-lsp)))
 
 (use-package insert-translated-name
   :quelpa (insert-translated-name :fetcher github :repo "manateelazycat/insert-translated-name")
