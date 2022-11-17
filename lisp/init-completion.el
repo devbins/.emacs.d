e3afe3589582997b87aabb1c82fe915ae91314bb
devbins <bin6160@126.com>
2022-11-17 21:11:10 +0800

    Feature: provide completion

---
+++ lisp/init-completion.el
@@ -0,0 +1,268 @@
+;;; init-completion.el --- -*- lexical-binding: t -*-
+;;
+;; Filename: init-completion.el
+;; Description:
+;; Author: devbins
+;; Maintainer:
+;; Copyright (C) 2019 devbins
+;; Created: Thu Nov 17 20:14:59 2022 (+0800)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 3
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+(use-package vertico
+  :hook (after-init . vertico-mode)
+  :config
+  (setq vertico-count 20
+        vertico-cycle t)
+  :hook (minibuffer-setup . vertico-repeat-save)
+  :bind
+  (:map vertico-map
+   ("C-j" . vertico-next)
+   ("C-k" . vertico-previous)
+   ("C-f" . vertico-exit)))
+
+(use-package vertico-directory
+  :after vertico
+  :ensure nil
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy)
+  :bind
+  (:map vertico-map
+   ("RET"   . 'vertico-directory-enter)
+   ("/"     . 'vertico-directory-enter)
+   ("DEL"   . 'vertico-directory-delete-char)
+   ("M-DEL" . 'vertico-directory-delete-word)))
+
+(use-package vertico-posframe
+  :after vertico
+  :hook (vertico-mode . vertico-posframe-mode)
+  :config
+  (setq vertico-posframe-parameters '((left-fringe . 8)
+                                      (right-fringe . 8)
+                                      (weight . 50))))
+
+(use-package orderless
+  :init
+  (setq completion-styles '(orderless basic)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package consult
+  :bind (;; C-c bindings (mode-specific-map)
+         ("C-c h" . consult-history)
+         ("C-c m" . consult-mode-command)
+         ("C-c b" . consult-bookmark)
+         ("C-c k" . consult-kmacro)
+         ;; C-x bindings (ctl-x-map)
+         ("C-x M-:" . consult-complex-command)
+         ("C-x b" . consult-buffer)
+         ("C-x 4 b" . consult-buffer-other-window)
+         ("C-x 5 b" . consult-buffer-other-frame)
+         ;; Custom M-# bindings for fast register access
+         ("M-#" . consult-register-load)
+         ("M-'" . consult-register-store)
+         ("C-M-#" . consult-register)
+         ;; Other custom bindings
+         ("M-y" . consult-yank-pop)
+         ("<help> a" . consult-apropos)
+         ;; M-g bindings (goto-map)
+         ("M-g e" . consult-compile-error)
+         ("M-g f" . consult-flymake)
+         ("M-g g" . consult-goto-line)
+         ("M-g M-g" . consult-goto-line)
+         ("M-g o" . consult-outline)
+         ("M-g m" . consult-mark)
+         ("M-g k" . consult-global-mark)
+         ("M-g i" . consult-imenu)
+         ("M-g I" . consult-imenu-multi)
+         ;; M-s bindings (search-map)
+         ("M-s f" . consult-find)
+         ("M-s F" . consult-locate)
+         ("M-s g" . consult-grep)
+         ("M-s G" . consult-git-grep)
+         ("M-s r" . consult-ripgrep)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi)
+         ("M-s m" . consult-multi-occur)
+         ("M-s k" . consult-keep-lines)
+         ("M-s u" . consult-focus-lines)
+         ;; Isearch integration
+         ("M-s e" . consult-isearch)
+         ([remap apropos]                       . consult-apropos)
+         ([remap bookmark-jump]                 . consult-bookmark)
+         ([remap evil-show-marks]               . consult-mark)
+         ([remap evil-show-registers]           . consult-register)
+         ([remap goto-line]                     . consult-goto-line)
+         ([remap imenu]                         . consult-imenu)
+         ([remap locate]                        . consult-locate)
+         ([remap load-theme]                    . consult-theme)
+         ([remap man]                           . consult-man)
+         ([remap recentf-open-files]            . consult-recent-file)
+         ([remap switch-to-buffer]              . consult-buffer)
+         ([remap switch-to-buffer-other-window] . consult-buffer-other-window)
+         ([remap switch-to-buffer-other-frame]  . consult-buffer-other-frame)
+         ([remap yank-pop]                      . consult-yank-pop)
+         :map isearch-mode-map
+         ("M-e" . consult-isearch)
+         ("M-s e" . consult-isearch)
+         ("M-s l" . consult-line)
+         ("M-s L" . consult-line-multi))
+  :init
+  (setq register-preview-delay 1
+        register-preview-function #'consult-register-format)
+
+  ;; Optionally tweak the register preview window.
+  ;; This adds thin lines, sorting and hides the mode line of the window.
+  (advice-add #'register-preview :override #'consult-register-window)
+
+  ;; Use Consult to select xref locations with preview
+  (setq xref-show-xrefs-function #'consult-xref
+        xref-show-definitions-function #'consult-xref)
+  :hook (completion-list-mode . consult-preview-at-point-mode)
+  :config
+  (consult-customize
+   consult-theme :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-file-register
+   consult--source-recent-file consult--source-project-recent-file
+   ;; :preview-key (kbd "M-.")
+   :preview-key '(:debounce 1 any)))
+
+(use-package consult-yasnippet
+  :after (consult yasnippet)
+  :bind ("M-s y" . consult-yasnippet))
+
+(use-package marginalia
+   :hook (after-init . marginalia-mode))
+
+(use-package all-the-icons-completion
+  :after marginalia
+  :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
+  :config
+  (all-the-icons-completion-mode))
+
+(use-package embark
+  :bind (("C-." . embark-act)
+         ("M-." . embark-dwim)
+         ("C-h B" . embark-bindings)))
+
+(use-package embark-consult
+  :after (embark consult)
+  :hook (embark-collect . consult-preview-at-point-mode))
+
+(use-package corfu
+  :bind (:map corfu-map
+         ("C-j" . corfu-next)
+         ("C-k" . corfu-previous)
+         ("C-f" . corfu-insert)
+         ("C-d" . corfu-info-documentation)
+         ("M-." . corfu-info-location))
+  :init
+  (setq corfu-auto t
+        corfu-cycle t
+        corfu-separator ?\s ;; orderless field separator
+        corfu-auto-delay 0.2
+        corfu-auto-prefix 1
+        corfu-quit-at-boundary t
+        corfu-preview-current nil
+        corfu-on-exact-match nil))
+
+(use-package corfu-doc
+  :after corfu
+  :bind (:map corfu-map
+         ("M-p" . corfu-doc-scroll-down)
+         ("M-n" . corfu-doc-scroll-up)
+         ("M-d" . corfu-doc-toggle))
+  :hook (corfu-mode . corfu-doc-mode))
+
+(use-package corfu-history
+  :ensure nil
+  :after corfu
+  :hook (corfu-mode . corfu-history-mode))
+
+(use-package cape
+  :init
+   ;; Add `completion-at-point-functions', used by `completion-at-point'.
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  (add-to-list 'completion-at-point-functions #'cape-history)
+  (add-to-list 'completion-at-point-functions #'cape-keyword)
+  (add-to-list 'completion-at-point-functions #'cape-tex)
+  (add-to-list 'completion-at-point-functions #'cape-sgml)
+  (add-to-list 'completion-at-point-functions #'cape-rfc1345)
+  (add-to-list 'completion-at-point-functions #'cape-abbrev)
+  (add-to-list 'completion-at-point-functions #'cape-symbol))
+
+;; Use Dabbrev with Corfu!
+(use-package dabbrev
+  ;; Swap M-/ and C-M-/
+  :bind (("M-/" . dabbrev-completion)
+         ("C-M-/" . dabbrev-expand))
+  ;; Other useful Dabbrev configurations.
+  :custom
+  (dabbrev-ignored-buffer-regexps '("\\.\\(?:pdf\\|jpe?g\\|png\\)\\'")))
+
+(use-package kind-icon
+  :ensure t
+  :after corfu
+  :custom
+  (kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
+(use-package corfu-english-helper
+  :quelpa (corfu-english-helper :fetcher github :repo "manateelazycat/corfu-english-helper")
+  :after corfu
+  :commands (toggle-corfu-english-helper corfu-english-helper-search)
+  :bind ("C-c t e" . 'toggle-corfu-english-helper))
+
+(use-package copilot
+  :quelpa (copilot :fetcher github :repo "zerolfx/copilot.el"
+                   :files ("*.el" "dist"))
+  :init
+  ;; accept completion from copilot and fallback to company
+  (defun my-tab ()
+    (interactive)
+    (or (copilot-accept-completion)
+        (corfu-complete))))
+
+(provide 'init-completion)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-completion.el ends here
