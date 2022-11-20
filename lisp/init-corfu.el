2d92515ed0f98b7ce6517cad1342449d4500aa61
devbins <bin6160@126.com>
2022-11-20 16:42:13 +0800

    Feature: use corfu replace company

---
+++ lisp/init-corfu.el
@@ -0,0 +1,145 @@
+;;; init-corfu.el --- -*- lexical-binding: t -*-
+;;
+;; Filename: init-corfu.el
+;; Description:
+;; Author: devbins
+;; Maintainer:
+;; Copyright (C) 2019 devbins
+;; Created: Fri Nov 18 10:16:42 2022 (+0800)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 5
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+(use-package corfu
+  :bind (:map corfu-map
+         ("C-j" . corfu-next)
+         ("C-k" . corfu-previous)
+         ("C-f" . corfu-insert)
+         ("C-d" . corfu-info-documentation)
+         ("M-." . corfu-info-location))
+  :init
+  (setq corfu-auto t
+        corfu-cycle t
+        corfu-separator ?\s ;; orderless field separator
+        corfu-auto-delay 0.2
+        corfu-auto-prefix 1
+        corfu-quit-at-boundary t
+        corfu-preview-current nil
+        corfu-on-exact-match nil))
+
+(use-package corfu-doc
+  :after corfu
+  :bind (:map corfu-map
+         ("M-p" . corfu-doc-scroll-down)
+         ("M-n" . corfu-doc-scroll-up)
+         ("M-d" . corfu-doc-toggle))
+  :hook (corfu-mode . corfu-doc-mode))
+
+(use-package corfu-history
+  :ensure nil
+  :after corfu
+  :hook (corfu-mode . corfu-history-mode))
+
+(use-package cape
+  :init
+   ;; Add `completion-at-point-functions', used by `completion-at-point'.
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  (add-to-list 'completion-at-point-functions #'cape-history)
+  (add-to-list 'completion-at-point-functions #'cape-keyword)
+  (add-to-list 'completion-at-point-functions #'cape-tex)
+  (add-to-list 'completion-at-point-functions #'cape-sgml)
+  (add-to-list 'completion-at-point-functions #'cape-rfc1345)
+  (add-to-list 'completion-at-point-functions #'cape-abbrev)
+  (add-to-list 'completion-at-point-functions #'cape-symbol))
+
+;; Use Dabbrev with Corfu!
+(use-package dabbrev
+  ;; Swap M-/ and C-M-/
+  :bind (("M-/" . dabbrev-completion)
+         ("C-M-/" . dabbrev-expand))
+  ;; Other useful Dabbrev configurations.
+  :custom
+  (dabbrev-ignored-buffer-regexps '("\\.\\(?:pdf\\|jpe?g\\|png\\)\\'")))
+
+(use-package kind-icon
+  :ensure t
+  :after corfu
+  :custom
+  (kind-icon-default-face 'corfu-default) ; to compute blended backgrounds correctly
+  :config
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
+(use-package corfu-english-helper
+  :quelpa (corfu-english-helper :fetcher github :repo "manateelazycat/corfu-english-helper")
+  :after corfu
+  :commands (toggle-corfu-english-helper corfu-english-helper-search)
+  :bind ("C-c t e" . 'toggle-corfu-english-helper))
+
+(use-package copilot
+  :quelpa (copilot :fetcher github :repo "zerolfx/copilot.el"
+                   :files ("*.el" "dist"))
+  :init
+  ;; accept completion from copilot and fallback to company
+  (defun my-tab ()
+    (interactive)
+    (or (copilot-accept-completion)
+        (corfu-complete))))
+
+
+;; install dependences
+;; brew install deno
+;; sudo pacman -S deno
+(use-package deno-bridge
+  :disabled
+  :quelpa (deno-bridge :fetcher github :repo "manateelazycat/deno-bridge"))
+
+(use-package insert-translated-name
+  :disabled
+  :quelpa (insert-translated-name :fetcher github :repo "manateelazycat/insert-translated-name" :files ("*"))
+  :bind ("C-c t t" . 'insert-translated-name-insert)
+  :commands (insert-translated-name-insert)
+  :init (setq insert-translated-name-translate-engine 'youdao)
+  :config
+  (defvar insert-translated-name-camel-style-mode-list
+    '(go-mode)))
+
+(provide 'init-corfu)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-corfu.el ends here
