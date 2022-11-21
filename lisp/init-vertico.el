82079b39e6b6c3d61dc60139c62f9cea0bd79ea8
devbins <bin6160@126.com>
2022-11-21 21:10:35 +0800

    Clean: delete dumplicate corfu in vertico

--- lisp/init-vertico.el
+++ lisp/init-vertico.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 165
+;;     Update #: 166
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -186,58 +186,6 @@
 (use-package embark-consult
   :after (embark consult)
   :hook (embark-collect . consult-preview-at-point-mode))
-
-(use-package corfu
-  :bind (:map corfu-map
-         ("C-j" . corfu-next)
-         ("C-k" . corfu-previous)
-         ("C-f" . corfu-insert)
-         ("C-d" . corfu-info-documentation)
-         ("M-." . corfu-info-location))
-  :init
-  (setq corfu-auto t
-        corfu-cycle t
-        corfu-separator ?\s ;; orderless field separator
-        corfu-auto-delay 0.2
-        corfu-auto-prefix 1
-        corfu-quit-at-boundary t
-        corfu-preview-current nil
-        corfu-on-exact-match nil))
-
-(use-package corfu-doc
-  :after corfu
-  :bind (:map corfu-map
-         ("M-p" . corfu-doc-scroll-down)
-         ("M-n" . corfu-doc-scroll-up)
-         ("M-d" . corfu-doc-toggle))
-  :hook (corfu-mode . corfu-doc-mode))
-
-(use-package corfu-history
-  :ensure nil
-  :after corfu
-  :hook (corfu-mode . corfu-history-mode))
-
-(use-package cape
-  :init
-   ;; Add `completion-at-point-functions', used by `completion-at-point'.
-  (add-to-list 'completion-at-point-functions #'cape-file)
-  (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-  (add-to-list 'completion-at-point-functions #'cape-history)
-  (add-to-list 'completion-at-point-functions #'cape-keyword)
-  (add-to-list 'completion-at-point-functions #'cape-tex)
-  (add-to-list 'completion-at-point-functions #'cape-sgml)
-  (add-to-list 'completion-at-point-functions #'cape-rfc1345)
-  (add-to-list 'completion-at-point-functions #'cape-abbrev)
-  (add-to-list 'completion-at-point-functions #'cape-symbol))
-
-;; Use Dabbrev with Corfu!
-(use-package dabbrev
-  ;; Swap M-/ and C-M-/
-  :bind (("M-/" . dabbrev-completion)
-         ("C-M-/" . dabbrev-expand))
-  ;; Other useful Dabbrev configurations.
-  :custom
-  (dabbrev-ignored-buffer-regexps '("\\.\\(?:pdf\\|jpe?g\\|png\\)\\'")))
 
 (provide 'init-vertico)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
