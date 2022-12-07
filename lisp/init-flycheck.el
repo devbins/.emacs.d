31fbf5974eabb5649301773d63cff87273f070b5
devbins <bin6160@126.com>
2022-12-07 08:09:59 +0800

    Feature: revert flycheck-indication-mode to default

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -54,9 +54,6 @@
               '(not text-mode outline-mode fundamental-mode lisp-interaction-mode
                     org-mode diff-mode shell-mode eshell-mode term-mode vterm-mode)
               flycheck-emacs-lisp-load-path 'inherit
-              flycheck-indication-mode (if (display-graphic-p)
-                                           'right-fringe
-                                         'right-margin)
               ;; Only check while saving and opening files
               flycheck-check-syntax-automatically '(save mode-enabled))
   :config
