0f9b4d9aac8220deceb52f42f1e41b21cd240ec4
devbins <bin6160@126.com>
2022-12-12 08:38:08 +0800

    Feature: optimize hydra

--- lisp/init-hydra.el
+++ lisp/init-hydra.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 3
+;;     Update #: 6
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -69,12 +69,13 @@
     (pretty-hydra-define toggles-hydra (:title (pretty-hydra-title "Toggles" 'faicon "toggle-on")
                                         :color amaranth :quit-key "q")
       ("Basic"
-       (("n" (if (fboundp 'display-line-numbers-mode)
-                 (display-line-numbers-mode (if display-line-numbers-mode -1 1))
-               (global-linum-mode (if global-linum-mode -1 1)))
-         "line number" :toggle (if (fboundp 'display-line-numbers-mode)
-                                   display-line-numbers-mode
-                                 global-linum-mode))
+       (("n" (cond ((fboundp 'display-line-numbers-mode)
+                    (display-line-numbers-mode (if display-line-numbers-mode -1 1)))
+                   ((fboundp 'gblobal-linum-mode)
+                    (global-linum-mode (if global-linum-mode -1 1))))
+                   "line number"
+                   :toggle (or (bound-and-true-p display-line-numbers-mode)
+                              (bound-and-true-p global-linum-mode)))
         ("a" global-aggressive-indent-mode "aggressive indent" :toggle t)
         ("h" global-hungry-delete-mode "hungry delete" :toggle t)
         ("e" electric-pair-mode "electric pair" :toggle t)
