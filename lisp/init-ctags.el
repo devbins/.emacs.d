1e5c279a9faf21a2aaf089d98e71525adaa9a590
devbins <bin6160@126.com>
2023-05-16 08:14:47 +0800

    Feature: optimized citre

--- lisp/init-ctags.el
+++ lisp/init-ctags.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 9
+;;     Update #: 10
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -48,58 +48,80 @@
 
 ;; Ctags IDE on the True Editor
 ;; @see https://github.com/universal-ctags/citre#quick-start
-(when emacs/>=26p
-  (use-package citre
-    :diminish
-    :bind (("C-x c j" . citre-jump+)
-           ("C-x c k" . citre-jump-back)
-           ("C-x c p" . citre-peek)
-           ("C-x c a" . citre-ace-peek)
-           ("C-x c u" . citre-update-this-tags-file))
-    :init
-    (require 'citre-config)
-    (setq citre-auto-enable-citre-mode-modes '(prog-mode))
+(use-package citre
+  :diminish
+  :commands citre-jump-back
+  :functions xref-go-back
+  :bind (:map prog-mode-map
+         ("C-x c j" . citre-jump+)
+         ("C-x c k" . citre-jump-back+)
+         ("C-x c p" . citre-peek)
+         ("C-x c a" . citre-ace-peek)
+         ("C-x c u" . citre-update-this-tags-file))
+  :init
+  (setq citre-auto-enable-citre-mode-modes '(prog-mode)
+        citre-default-create-tags-file-location 'global-cache
+        citre-use-project-root-when-creating-tags t
+        citre-prompt-language-for-ctags-command t)
 
-    (defun citre-jump+ ()
-      "Jump to the definition of the symbol at point.
+  (defun citre-jump+ ()
+    "Jump to the definition of the symbol at point.
 Fallback to `xref-find-definitions'."
-      (interactive)
-      (condition-case _
-          (citre-jump)
-        (error (call-interactively #'xref-find-definitions))))
-    :config
-    (with-no-warnings
-      (with-eval-after-load 'projectile
-        (setq citre-project-root-function #'projectile-project-root))
+    (interactive)
+    (condition-case _
+        (citre-jump)
+      (error (call-interactively #'xref-find-definitions))))
 
-      ;; Integrate with `lsp-mode' and `eglot'
-      (define-advice xref--create-fetcher (:around (fn &rest args) fallback)
-        (let ((fetcher (apply fn args))
-              (citre-fetcher
-               (let ((xref-backend-functions '(citre-xref-backend t)))
-                 (ignore xref-backend-functions)
-                 (apply fn args))))
-          (lambda ()
-            (or (with-demoted-errors "%s, fallback to citre"
-                  (funcall fetcher))
-                (funcall citre-fetcher)))))
+  (defun citre-jump-back+ ()
+    "Go back to the position before last `citre-jump'.
+Fallback to `xref-go-back'."
+    (interactive)
+    (condition-case _
+        (citre-jump-back)
+      (error (if (fboundp #'xref-go-back)
+                 (call-interactively #'xref-go-back)
+               (call-interactively #'xref-pop-marker-stack)))))
+  :config
+  (with-eval-after-load 'cc-mode (require 'citre-lang-c))
+  (with-eval-after-load 'dired (require 'citre-lang-fileref))
+  (with-eval-after-load 'verilog-mode (require 'citre-lang-verilog))
 
-      (defun lsp-citre-capf-function ()
-        "A capf backend that tries lsp first, then Citre."
-        (let ((lsp-result (lsp-completion-at-point)))
-          (if (and lsp-result
+  (with-no-warnings
+    ;; Use Citre xref backend as a fallback
+    (define-advice xref--create-fetcher (:around (fn &rest args) fallback)
+      (let ((fetcher (apply fn args))
+            (citre-fetcher
+             (let ((xref-backend-functions '(citre-xref-backend t)))
+               (ignore xref-backend-functions)
+               (apply fn args))))
+        (lambda ()
+          (or (with-demoted-errors "%s, fallback to citre"
+                (funcall fetcher))
+              (funcall citre-fetcher)))))
+
+    ;; Combine completions from Citre and lsp
+    (defun lsp-citre-capf-function ()
+      "A capf backend that tries lsp first, then Citre."
+      (let ((lsp-result (cond
+                         ((bound-and-true-p lsp-mode)
+                          (and (fboundp #'lsp-completion-at-point)
+                               (lsp-completion-at-point)))
+                         ((bound-and-true-p eglot--managed-mode)
+                          (and (fboundp #'eglot-completion-at-point)
+                               (eglot-completion-at-point))))))
+        (if (and lsp-result
                  (try-completion
                   (buffer-substring (nth 0 lsp-result)
                                     (nth 1 lsp-result))
                   (nth 2 lsp-result)))
-              lsp-result
-            (citre-completion-at-point))))
+            lsp-result
+          (citre-completion-at-point))))
 
-      (defun enable-lsp-citre-capf-backend ()
-        "Enable the lsp + Citre capf backend in current buffer."
-        (add-hook 'completion-at-point-functions #'lsp-citre-capf-function nil t))
+    (defun enable-lsp-citre-capf-backend ()
+      "Enable the lsp + Citre capf backend in current buffer."
+      (add-hook 'completion-at-point-functions #'lsp-citre-capf-function nil t))
 
-      (add-hook 'citre-mode-hook #'enable-lsp-citre-capf-backend))))
+    (add-hook 'citre-mode-hook #'enable-lsp-citre-capf-backend)))
 
 (provide 'init-ctags)
 
