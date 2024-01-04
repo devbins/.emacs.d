8864e2c783a5a260703185188637fc4d7d7da26d
devbin <8175896+devbins@users.noreply.github.com>
2024-01-04 23:03:23 +0800

    Feature: support ollama

---
+++ lisp/init-ai.el
@@ -0,0 +1,62 @@
+;;; init-ai.el --- -*- lexical-binding: t -*-
+;;
+;; Filename: init-ai.el
+;; Description:
+;; Author: binsheng
+;; Maintainer:
+;; Copyright (C) 2019 binsheng
+;; Created: Wed Jan  3 10:39:11 2024 (+0800)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 38
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+
+(use-package ellama
+  :init
+  (setopt ellama-language "Chinese"
+                ellama-auto-scroll t)
+  (require 'llm-ollama)
+  :config
+  (setopt ellama-provider
+		  (make-llm-ollama
+		   :chat-model "mistral:latest" :embedding-model "mistral:latest")))
+
+(provide 'init-ai)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-ai.el ends here
