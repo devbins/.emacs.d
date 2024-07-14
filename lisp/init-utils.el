56ee45346a35da995eeeeb6eff48d5d03c6abb89
devbin <8175896+devbins@users.noreply.github.com>
2024-07-14 08:15:43 +0800

    Fix: Fix fullscreen issue on macOS by adjusting window info

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -288,7 +288,14 @@
   :init
   (setq holo-layer-type-animation-style "lightning"
         holo-layer-enable-cursor-animation t
-        holo-layer-enable-type-animation t))
+        holo-layer-enable-type-animation t)
+  :config
+  (defun holo-layer-mac-get-window-info (orig-fn &rest args)
+    (if (memq (frame-parameter (car args) 'fullscreen)
+              '(fullscreen fullboth maximized))
+        (list 0 38 (frame-pixel-width) (frame-pixel-height) 0)
+      (apply orig-fn args)))
+  (advice-add 'holo-layer-get-emacs-frame-info :around #'holo-layer-mac-get-window-info))
 
 (provide 'init-utils)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
