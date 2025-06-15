58e1b16353e3bf60d74dfca2df67312ab98406ff
devbin <8175896+devbins@users.noreply.github.com>
2025-06-15 10:53:20 +0800

    Refactor: Configure web-mode for Vue and disable auto-indentation

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -137,9 +137,13 @@
 (use-package web-mode
   :mode "\\.\\(phtml\\|php|[gj]sp\\|as[cp]x\\|erb\\|djhtml\\|html?\\|hbs\\|ejs\\|jade\\|swig\\|tm?pl\\|vue\\)$"
   :config
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
+  (setq web-mode-content-types-alist '(("vue" . "\\.vue\\'"))
+        web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2
+        web-mode-enable-auto-indentation nil ;; 禁止粘贴时格式化代码
+        ))
+
 
 (use-package haml-mode)
 (use-package php-mode)
