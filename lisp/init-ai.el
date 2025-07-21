75780b23a025b2ecf6814c7633233179fc6c35a8
devbin <8175896+devbins@users.noreply.github.com>
2025-07-22 07:54:59 +0800

    Style: Reformat gptel-translate-to-chinese function

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 264
+;;     Update #: 266
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -88,23 +88,23 @@
                    "gptel proxy disabled"
                  "enabled gptel proxy")))
 
-(defun gptel-translate-to-chinese(&optional dry-run)
-  "Use AI to translate the currently selected text into Chinese."
-  (interactive "P")
-  (gptel-request (list (or (get-char-property (point) 'gptel-rewrite)
-                          (buffer-substring-no-properties (region-beginning) (region-end)))
-                       "What is the required change?"
-                       "Rewrite:")
-    :dry-run dry-run
-    :system (alist-get 'translate gptel-directives)
-    :stream t
-    :context
-    (let ((ov (or (cdr-safe (get-char-property-and-overlay (point) 'gptel-rewrite))
-                 (make-overlay (region-beginning) (region-end) nil t))))
-      (overlay-put ov 'category 'gptel)
-      (overlay-put ov 'evaporate t)
-      (cons ov (generate-new-buffer "*gptel-rewrite*")))
-    :callback #'gptel--rewrite-callback))
+    (defun gptel-translate-to-chinese(&optional dry-run)
+      "Use AI to translate the currently selected text into Chinese."
+      (interactive "P")
+      (gptel-request (list (or (get-char-property (point) 'gptel-rewrite)
+                               (buffer-substring-no-properties (region-beginning) (region-end)))
+                           "What is the required change?"
+                           "Rewrite:")
+                     :dry-run dry-run
+                     :system (alist-get 'translate gptel-directives)
+                     :stream t
+                     :context
+        (let ((ov (or (cdr-safe (get-char-property-and-overlay (point) 'gptel-rewrite))
+                      (make-overlay (region-beginning) (region-end) nil t))))
+          (overlay-put ov 'category 'gptel)
+          (overlay-put ov 'evaporate t)
+          (cons ov (generate-new-buffer "*gptel-rewrite*")))
+        :callback #'gptel--rewrite-callback))
 
 (with-eval-after-load 'gptel-transient
   (transient-append-suffix 'gptel-menu '(2 -1)
