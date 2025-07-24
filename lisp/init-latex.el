7c637de64cdfd7a4eb225a638b5b87c24ba8f11c
devbin <8175896+devbins@users.noreply.github.com>
2025-07-24 21:26:58 +0800

    Feature: Add LaTeX configuration with AUCTeX support

---
+++ lisp/init-latex.el
@@ -0,0 +1,67 @@
+;;; init-latex.el --- -*- lexical-binding: t -*-
+;;
+;; Filename: init-latex.el
+;; Description: latex config
+;; Author: devbins
+;; Maintainer:
+;; Copyright (C) 2019 devbins
+;; Created: Thu Jul 24 20:59:33 2025 (+0800)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 19
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+(use-package tex
+  :ensure auctex
+  :custom
+  (TeX-parse-self t) ; 自动解析 tex 文件
+  (TeX-PDF-mode t)
+  (TeX-DVI-via-PDFTeX t)
+  (TeX-engine 'xetex) ;; 支持中文
+  :hook (latex-mode . (lambda () (turn-on-cdlatex)
+                        (turn-on-reftex))))
+
+(use-package cdlatex
+  :hook ((org-mode . (lambda () (turn-on-org-cdlatex)
+                       ;; (org-latex-preview-auto-mode)
+                       ))
+         (LaTeX-mode . turn-on-cdlatex)))
+
+(provide 'init-latex)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-latex.el ends here
