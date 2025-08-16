dcbc677e0d9feddb397cf95c5bf89f3d58459639
devbin <8175896+devbins@users.noreply.github.com>
2025-08-17 07:24:35 +0800

    Feature: Configure mu4e-views for enhanced email handling

--- lisp/init-mail.el
+++ lisp/init-mail.el
@@ -221,6 +221,20 @@
                '(:empty . (:name "Empty"
                            :shortname ""
                            :function (lambda (msg) "  ")))))
+(use-package mu4e-views
+  :after mu4e
+  :bind (:map mu4e-headers-mode-map
+	     ("v" . mu4e-views-mu4e-select-view-msg-method) ;; select viewing method
+	     ("M-n" . mu4e-views-cursor-msg-view-window-down) ;; from headers window scroll the email view
+	     ("M-p" . mu4e-views-cursor-msg-view-window-up) ;; from headers window scroll the email view
+         ("f" . mu4e-views-toggle-auto-view-selected-message) ;; toggle opening messages automatically when moving in the headers view
+         ("i" . mu4e-views-mu4e-view-as-nonblocked-html)) ;; show currently selected email with all remote content
+  :config
+  ;; (setq mu4e-views-completion-method 'ivy) ;; use ivy for completion
+  (setq mu4e-views-default-view-method "html") ;; make xwidgets default
+  (mu4e-views-mu4e-use-view-msg-method "html") ;; select the default
+  (setq mu4e-views-next-previous-message-behaviour 'stick-to-current-window) ;; when pressing n and p stay in the current window
+  (setq mu4e-views-auto-view-selected-message t)) ;; automatically open messages when moving in the headers view
 (provide 'init-mail)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; init-mail.el ends here
