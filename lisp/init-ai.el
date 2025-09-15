51f392d1133d71508fa52abf2c28b28ed7cf4419
devbin <8175896+devbins@users.noreply.github.com>
2025-09-15 08:23:58 +0800

    Fix: Update Modelscope models

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 323
+;;     Update #: 333
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -135,7 +135,8 @@
 
   (gptel-make-openai "modelscope"
     :key (auth-source-pass-get 'secret "modelscope")
-    :models '(Qwen/Qwen3-Coder-480B-A35B-Instruct
+    :models '(Qwen/Qwen3-Next-80B-A3B-Instruct
+              Qwen/Qwen3-Coder-480B-A35B-Instruct
               Qwen/Qwen3-235B-A22B-Instruct-2507
               Qwen/Qwen3-235B-A22B-Thinking-2507)
     :host "api-inference.modelscope.cn"
