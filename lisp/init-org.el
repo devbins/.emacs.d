51b78f1f06885a951cfa6b7f32b3efd69e9a586b
devbin <8175896+devbins@users.noreply.github.com>
2025-09-26 08:12:20 +0800

    Refactor: Remove Babel result length limit

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 1025
+;;     Update #: 1027
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -938,31 +938,6 @@ prepended to the element after the #+HEADER: tag."
               )
             )))))
   (advice-add 'org-babel-execute-src-block :before #'get-attributes-from-src-block-result)
-
-  ;; limit the babel result length
-  (defvar org-babel-result-lines-limit 40)
-  (defvar org-babel-result-length-limit 6000)
-
-  (defun org-babel-insert-result@limit (orig-fn result &rest args)
-    (if (not (member (car (org-babel-get-src-block-info)) '("jupyter-python"))) ; not for jupyter-python etc.
-        (if (and result (or org-babel-result-lines-limit org-babel-result-length-limit))
-            (let (new-result plines plenght limit)
-              (with-temp-buffer
-                (insert result)
-                (setq plines (if org-babel-result-lines-limit
-                                 (goto-line org-babel-result-lines-limit)
-                               (point-max)))
-                (setq plenght (if org-babel-result-length-limit
-                                  (min org-babel-result-length-limit (point-max))
-                                (point-max)))
-                (setq limit (min plines plenght))
-                (setq new-result (concat (buffer-substring (point-min) limit)
-                                         (if (< limit (point-max)) "..."))))
-              (apply orig-fn new-result args))
-          (apply orig-fn result args))
-      (apply orig-fn result args)))
-
-  (advice-add 'org-babel-insert-result :around #'org-babel-insert-result@limit)
 
   ;; 代码块语法高亮
   (setq org-src-fontify-natively t
