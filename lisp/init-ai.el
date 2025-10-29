c3d737b8adb29880b06594cc5fbdc1d5ef6ff688
devbin <8175896+devbins@users.noreply.github.com>
2025-10-29 09:17:06 +0800

    Feature: disable byte-compile init-ai

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -1,4 +1,4 @@
-;;; init-ai.el --- -*- lexical-binding: t -*-
+;;; init-ai.el --- -*- lexical-binding: t no-byte-compile: t -*-
 ;;
 ;; Filename: init-ai.el
 ;; Description:
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 341
+;;     Update #: 342
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
