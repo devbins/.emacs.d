8121d36dea1fac2c679b14c13818b7dea4ccb3b7
devbin <8175896+devbins@users.noreply.github.com>
2026-04-14 21:25:39 +0800

    Refactor: Update gt package to expose translator command

--- lisp/init-dict.el
+++ lisp/init-dict.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 52
+;;     Update #: 57
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -65,6 +65,7 @@
 
 (use-package gt
   :bind ("C-c t e" . gt-translate)
+  :commands (gt-translator)
   :init
   (setq gt-langs '(en zh)
         gt-buffer-render-follow-p t
@@ -77,59 +78,58 @@
         gt-pop-posframe-backcolor (face-background 'tooltip nil t)
         gt-pin-posframe-bdcolor (face-background 'posframe-border nil t))
   :config
-  (with-no-warnings
-    (setq gt-preset-translators
-          `((default . ,(gt-translator
-                         :taker   (list (gt-taker :pick nil :if 'selection)
-                                        (gt-taker :text 'paragraph :if '(Info-mode help-mode helpful-mode devdocs-mode))
-                                        (gt-taker :text 'buffer :pick 'fresh-word
-                                                  :if (lambda (translatror)
-                                                        (and (not (derived-mode-p 'fanyi-mode)) buffer-read-only)))
-                                        (gt-taker :text 'word))
-                         :engines (if (display-graphic-p)
-                                      (list (gt-bing-engine :if 'not-word)
-                                            (gt-youdao-dict-engine :if 'word))
+  (setq gt-preset-translators
+        `((default . ,(gt-translator
+                       :taker   (list (gt-taker :pick nil :if 'selection)
+                                      (gt-taker :text 'paragraph :if '(Info-mode help-mode helpful-mode devdocs-mode))
+                                      (gt-taker :text 'buffer :pick 'fresh-word
+                                                :if (lambda (translatror)
+                                                      (and (not (derived-mode-p 'fanyi-mode)) buffer-read-only)))
+                                      (gt-taker :text 'word))
+                       :engines (if (display-graphic-p)
                                     (list (gt-bing-engine :if 'not-word)
-                                          (gt-youdao-dict-engine :if 'word)
-                                          (gt-youdao-suggest-engine :if 'word)
-                                          (gt-google-engine :if 'word)))
-                         :render  (list (gt-posframe-pop-render
-                                         :if (lambda (translator)
-                                               (and (display-graphic-p)
-                                                    (not (derived-mode-p 'Info-mode 'help-mode 'helpful-mode 'devdocs-mode))
-                                                    (not (member (buffer-name) '("COMMIT_EDITMSG")))))
-                                         :frame-params (list :accept-focus nil
-                                                             :width 70
-                                                             :height 15
-                                                             :left-fringe 16
-                                                             :right-fringe 16
-                                                             :border-width 1
-                                                             :border-color gt-pin-posframe-bdcolor))
-                                        (gt-overlay-render :if 'read-only)
-                                        (gt-insert-render :if (lambda (translator) (member (buffer-name) '("COMMIT_EDITMSG"))))
-                                        (gt-buffer-render))))
-            (multi-dict . ,(gt-translator :taker (gt-taker :prompt t)
-                                          :engines (list (gt-bing-engine)
-                                                         (gt-youdao-dict-engine)
-                                                         (gt-youdao-suggest-engine :if 'word)
-                                                         (gt-google-engine))
-                                          :render (gt-buffer-render)))
-            (Text-Utility . ,(gt-text-utility :taker (gt-taker :pick nil)
-                                              :render (gt-buffer-render)))))
+                                          (gt-youdao-dict-engine :if 'word))
+                                  (list (gt-bing-engine :if 'not-word)
+                                        (gt-youdao-dict-engine :if 'word)
+                                        (gt-youdao-suggest-engine :if 'word)
+                                        (gt-google-engine :if 'word)))
+                       :render  (list (gt-posframe-pop-render
+                                       :if (lambda (translator)
+                                             (and (display-graphic-p)
+                                                  (not (derived-mode-p 'Info-mode 'help-mode 'helpful-mode 'devdocs-mode))
+                                                  (not (member (buffer-name) '("COMMIT_EDITMSG")))))
+                                       :frame-params (list :accept-focus nil
+                                                           :width 70
+                                                           :height 15
+                                                           :left-fringe 16
+                                                           :right-fringe 16
+                                                           :border-width 1
+                                                           :border-color gt-pin-posframe-bdcolor))
+                                      (gt-overlay-render :if 'read-only)
+                                      (gt-insert-render :if (lambda (translator) (member (buffer-name) '("COMMIT_EDITMSG"))))
+                                      (gt-buffer-render))))
+          (multi-dict . ,(gt-translator :taker (gt-taker :prompt t)
+                                        :engines (list (gt-bing-engine)
+                                                       (gt-youdao-dict-engine)
+                                                       (gt-youdao-suggest-engine :if 'word)
+                                                       (gt-google-engine))
+                                        :render (gt-buffer-render)))
+          (Text-Utility . ,(gt-text-utility :taker (gt-taker :pick nil)
+                                            :render (gt-buffer-render)))))
 
-    (defun gt--translate (dict)
-      "Translate using DICT from the preset tranlators."
-      (gt-start (alist-get dict gt-preset-translators)))
+  (defun gt--translate (dict)
+    "Translate using DICT from the preset tranlators."
+    (gt-start (alist-get dict gt-preset-translators)))
 
-    (defun gt-translate-prompt ()
-      "Translate with prompt using the multiple dictionaries."
-      (interactive)
-      (gt--translate 'multi-dict))
+  (defun gt-translate-prompt ()
+    "Translate with prompt using the multiple dictionaries."
+    (interactive)
+    (gt--translate 'multi-dict))
 
-    (defun gt-use-text-utility ()
-      "Handle the texts with the utilities."
-      (interactive)
-      (gt--translate 'Text-Utility))))
+  (defun gt-use-text-utility ()
+    "Handle the texts with the utilities."
+    (interactive)
+    (gt--translate 'Text-Utility)))
 
 ;; https://qiqijin.com/cn/dictionary-overlay.html
 (use-package dictionary-overlay
