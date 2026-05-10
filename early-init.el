f313eb8379742a48db47bdb08a455b8f6a850d0a
devbin <8175896+devbins@users.noreply.github.com>
2026-05-10 10:23:48 +0800

    Refactor: Improve native-comp initialization settings

--- early-init.el
+++ early-init.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 22
+;;     Update #: 23
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -51,6 +51,7 @@
           comp-speed 3
           warning-minimum-level :error
           comp-deferred-compilation t
+          native-comp-async-jobs-number 2
           native-comp-async-report-warnings-errors 'slient)
      (message "Not support native-comp"))
 
