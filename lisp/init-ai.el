dcfe79df15f2228081f2af50a7fdacd79e0e04e8
devbin <8175896+devbins@users.noreply.github.com>
2026-06-14 09:18:47 +0800

    Feature: set global-agent-shell-pet-mode command

--- lisp/init-ai.el
+++ lisp/init-ai.el
@@ -368,6 +368,7 @@
 
   (use-package agent-shell-pet
     :load-path "site-lisp/agent-shell-pet"
+    :commands (global-agent-shell-pet-mode)
     :init (global-agent-shell-pet-mode 1)
     :config
     (setq agent-shell-pet-renderer (if sys/macp 'macos-native 'child-frame)
