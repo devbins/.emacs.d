ad333227fae25cea2d6fd32663abd131a9c5b4a2
devbin <8175896+devbins@users.noreply.github.com>
2026-08-13 21:26:36 +0800

    Chore: Comment out markdown-ts-mode configuration

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -160,49 +160,48 @@ mermaid.initialize({
                                  (auth-source-user-and-password "api.github.com"))))
       (setq grip-github-user (car credential)
             grip-github-password (cadr credential)))))
-(if emacs/>=31p
-    (use-package markdown-ts-mode :ensure nil)
-  (use-package markdown-ts-mode
-    :mode ("\\.md\\'" . markdown-ts-mode)
-    :bind (:map markdown-ts-mode-map ("<tab>" . markdown-cycle))
-    :config
-    (add-to-list 'treesit-language-source-alist '(markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown/src"))
-    (add-to-list 'treesit-language-source-alist '(markdown-inline "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown-inline/src"))
-    (setq markdown-ts--treesit-settings
-          (treesit-font-lock-rules
-           :language 'markdown-inline
-           :override t
-           :feature 'delimiter
-           '([ "[" "]" "(" ")" ] @shadow)
 
-           :language 'markdown
-           :feature 'paragraph
-           '([((setext_heading) @font-lock-function-name-face)
-              ((atx_heading) @font-lock-function-name-face)
-              ((thematic_break) @shadow)
-              ((indented_code_block) @font-lock-comment-face)
-              (list_item (list_marker_star) @font-lock-constant-face)
-              (list_item (list_marker_plus) @font-lock-constant-face)
-              (list_item (list_marker_minus) @font-lock-constant-face)
-              (list_item (list_marker_dot) @font-lock-constant-face)
-              (fenced_code_block (fenced_code_block_delimiter) @font-lock-doc-face)
-              (fenced_code_block (code_fence_content) @font-lock-comment-face)
-              ((block_quote_marker) @font-lock-comment-face)
-              (block_quote (paragraph) @font-lock-comment-face)
-              (block_quote (block_quote_marker) @font-lock-comment-face)
-              ])
+;; (use-package markdown-ts-mode :ensure nil
+;;   :mode ("\\.md\\'" . markdown-ts-mode)
+;;   :bind (:map markdown-ts-mode-map ("<tab>" . markdown-cycle))
+;;   :config
+;;   (add-to-list 'treesit-language-source-alist '(markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown/src"))
+;;   (add-to-list 'treesit-language-source-alist '(markdown-inline "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown-inline/src"))
+;;   (setq markdown-ts--treesit-settings
+;;         (treesit-font-lock-rules
+;;          :language 'markdown-inline
+;;          :override t
+;;          :feature 'delimiter
+;;          '([ "[" "]" "(" ")" ] @shadow)
 
-           :language 'markdown-inline
-           :feature 'paragraph-inline
-           '([
-              ((image_description) @link)
-              ((link_destination) @font-lock-comment-face)
-              ((code_span) @font-lock-comment-face)
-              ((emphasis) @underline)
-              ((strong_emphasis) @bold)
-              (inline_link (link_text) @link)
-              (inline_link (link_destination) @font-lock-comment-face)
-              (shortcut_link (link_text) @link)])))))
+;;          :language 'markdown
+;;          :feature 'paragraph
+;;          '([((setext_heading) @font-lock-function-name-face)
+;;             ((atx_heading) @font-lock-function-name-face)
+;;             ((thematic_break) @shadow)
+;;             ((indented_code_block) @font-lock-comment-face)
+;;             (list_item (list_marker_star) @font-lock-constant-face)
+;;             (list_item (list_marker_plus) @font-lock-constant-face)
+;;             (list_item (list_marker_minus) @font-lock-constant-face)
+;;             (list_item (list_marker_dot) @font-lock-constant-face)
+;;             (fenced_code_block (fenced_code_block_delimiter) @font-lock-doc-face)
+;;             (fenced_code_block (code_fence_content) @font-lock-comment-face)
+;;             ((block_quote_marker) @font-lock-comment-face)
+;;             (block_quote (paragraph) @font-lock-comment-face)
+;;             (block_quote (block_quote_marker) @font-lock-comment-face)
+;;             ])
+
+;;          :language 'markdown-inline
+;;          :feature 'paragraph-inline
+;;          '([
+;;             ((image_description) @link)
+;;             ((link_destination) @font-lock-comment-face)
+;;             ((code_span) @font-lock-comment-face)
+;;             ((emphasis) @underline)
+;;             ((strong_emphasis) @bold)
+;;             (inline_link (link_text) @link)
+;;             (inline_link (link_destination) @font-lock-comment-face)
+;;             (shortcut_link (link_text) @link)]))))
 
 (provide 'init-markdown)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
