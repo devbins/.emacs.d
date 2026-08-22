4e7e80c9c7d7cf7744efd07396c3e1483747e7ae
devbin <8175896+devbins@users.noreply.github.com>
2026-08-22 16:21:08 +0800

    Feature: Add appine-grab-url command

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 342
+;;     Update #: 345
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -311,7 +311,7 @@
   (lolipop-mode))
 
 (use-package appine
-  :commands (appine appine-open-url appine-open-file appine-open-file-from-recentf appine-rss)
+  :commands (appine appine-open-url appine-open-file appine-open-file-from-recentf appine-rss appine-grab-url)
   :load-path "site-lisp/appine"
   :config (setq appine-rss-path (expand-file-name "elfeed-feeds.org" user-emacs-directory)))
 
